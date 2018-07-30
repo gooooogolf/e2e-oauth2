7cf5926972a7cda5f291878f242d37d3c767cc5f
Gooooogolf <sirimongkol.pan@gmail.com>
2018-07-30 10:26:56 +0700

    Config enable sever with oauth2 and using jwt

--- auth-server/src/main/java/com/gooooogolf/auth/WebSecurityConfig.java
+++ auth-server/src/main/java/com/gooooogolf/auth/WebSecurityConfig.java
@@ -1,20 +1,32 @@
 package com.gooooogolf.auth;
 
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
 
 @Configuration
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .ldapAuthentication()
-                .userDnPatterns("uid={0},ou=people")
-                .groupSearchBase("ou=groups")
+        auth.ldapAuthentication()
+                .userSearchFilter("uid={0}")
                 .contextSource()
                 .url("ldap://localhost:8389/dc=springframework,dc=org")
                 .and()
@@ -25,8 +37,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
+        http.authorizeRequests()
                 .anyRequest().fullyAuthenticated()
                 .and()
                 .formLogin();
